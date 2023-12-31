01fb6c149538dfed750317ac305a631ea9abe07e
reaperbots <148609775+reaperbots@users.noreply.github.com>
2024-01-01 04:23:24 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -166,7 +166,7 @@ def get_readable_message():
         globals()['STATUS_START'] = STATUS_LIMIT * (PAGES - 1)
         globals()['PAGE_NO'] = PAGES
     for download in list(download_dict.values())[STATUS_START:STATUS_LIMIT+STATUS_START]:
-        msg += f"📂{escape(f'{download.name()}')}\n"
+        msg += f"📂 {escape(f'{download.name()}')}\n"
         msg += f"\n<b>{download.status()}...</b>"
         if download.status() not in [MirrorStatus.STATUS_SPLITTING, MirrorStatus.STATUS_SEEDING]:
             msg += f"\n{get_progress_bar_string(download.progress())} {download.progress()}"
