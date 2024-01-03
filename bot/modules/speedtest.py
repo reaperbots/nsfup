98cc12b74f988bcbe261915af8a94e6b580fca39
reaperbots <148609775+reaperbots@users.noreply.github.com>
2024-01-04 01:38:35 +0530

    Update speedtest.py

--- bot/modules/speedtest.py
+++ bot/modules/speedtest.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 from speedtest import Speedtest
 from pyrogram.handlers import MessageHandler
 from pyrogram.filters import command
@@ -11,31 +12,44 @@ from bot.helper.ext_utils.bot_utils import get_readable_file_size, new_task
 @new_task
 async def speedtest(_, message):
     speed = await sendMessage(message, "Initializing Speedtest...")
-    
-    def get_speedtest_results():
-        test = Speedtest()
-        test.get_best_server()
-        test.download()
-        test.upload()
-        return test.results
-    
-    result = await bot.loop.run_in_executor(None, get_speedtest_results)
-    
-    if not result:
-        await editMessage(speed, "Speedtest failed to complete.")
-        return
-    
-    string_speed  = f"<b>SPEEDTEST INFO</b>\n\n"
-    string_speed += f"<b>• Ping:</b> <code>{result.ping} ms</code>\n"
-    string_speed += f"<b>• Upload:</b> <code>{get_readable_file_size(result.upload / 8)}/s</code>\n"
-    string_speed += f"<b>• Download:</b> <code>{get_readable_file_size(result.download / 8)}/s</code>\n"
-    string_speed += f"<b>• IP Address:</b> <code>{result.client['ip']}</code>"
+    test = Speedtest()
+    test.get_best_server()
+    test.download()
+    test.upload()
+    test.results.share()
+    result = test.results.dict()
+    path = result['share']
+    string_speed = f'''
+<b>SPEEDTEST INFO</b>
+<b>• Upload:</b> <code>{get_readable_file_size(result['upload'] / 8)}/s</code>
+<b>• Download:</b>  <code>{get_readable_file_size(result['download'] / 8)}/s</code>
+<b>• Ping:</b> <code>{result['ping']} ms</code>
+<b>• Time:</b> <code>{result['timestamp']}</code>
+<b>• Data Sent:</b> <code>{get_readable_file_size(int(result['bytes_sent']))}</code>
+<b>• Data Received:</b> <code>{get_readable_file_size(int(result['bytes_received']))}</code>
 
+<b>SPEEDTEST SERVER</b>
+<b>• Name:</b> <code>{result['server']['name']}</code>
+<b>• Country:</b> <code>{result['server']['country']}, {result['server']['cc']}</code>
+<b>• Sponsor:</b> <code>{result['server']['sponsor']}</code>
+<b>• Latency:</b> <code>{result['server']['latency']}</code>
+<b>• Latitude:</b> <code>{result['server']['lat']}</code>
+<b>• Longitude:</b> <code>{result['server']['lon']}</code>
+
+<b>CLIENT DETAILS</b>
+<b>• IP Address:</b> <code>{result['client']['ip']}</code>
+<b>• Latitude:</b> <code>{result['client']['lat']}</code>
+<b>• Longitude:</b> <code>{result['client']['lon']}</code>
+<b>• Country:</b> <code>{result['client']['country']}</code>
+<b>• ISP:</b> <code>{result['client']['isp']}</code>
+<b>• ISP Rating:</b> <code>{result['client']['isprating']}</code>
+'''
     try:
-        await sendMessage(message, string_speed, photo=result.share())
+        pho = await sendMessage(message, string_speed, photo=path)
         await deleteMessage(speed)
     except Exception as e:
         LOGGER.error(str(e))
-        await editMessage(speed, string_speed)
+        pho = await editMessage(speed, string_speed)
 
-bot.add_handler(MessageHandler(speedtest, filters=command(BotCommands.SpeedCommand) & CustomFilters.authorized))
+bot.add_handler(MessageHandler(speedtest, filters=command(
+    BotCommands.SpeedCommand) & CustomFilters.authorized))
