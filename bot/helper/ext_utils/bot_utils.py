36d70a9e489493ad64852fb104d4287010bedfd1
reaperbots <148609775+reaperbots@users.noreply.github.com>
2024-01-05 01:09:02 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -166,17 +166,17 @@ def get_readable_message():
         globals()['STATUS_START'] = STATUS_LIMIT * (PAGES - 1)
         globals()['PAGE_NO'] = PAGES
     for download in list(download_dict.values())[STATUS_START:STATUS_LIMIT+STATUS_START]:
-        msg += f"📂 <b>{escape(f'{download.name()}')}</b>\n"
+        msg += f"<b>⌬ {escape(f'{download.name()}')}</b>\n"
         msg += f"\n<b>┌{download.status()}...</b>"
         if download.status() not in [MirrorStatus.STATUS_SPLITTING, MirrorStatus.STATUS_SEEDING]:
-            msg += f"\n<b><b>├{get_progress_bar_string(download.progress())} {download.progress()}</b>"
-            msg += f"\n<b><b>├Processed:</b> {download.processed_bytes()} of {download.size()}"
+            msg += f"\n<b>├{get_progress_bar_string(download.progress())} {download.progress()}</b>"
+            msg += f"\n<b>├Processed:</b> {download.processed_bytes()} of {download.size()}"
             msg += f"\n<b>├Speed:</b> {download.speed()}"
+            msg += f'<b> | ETA:</b> {download.eta()}'
             msg += f"\n<b>├User:</b> {source(download)}"
-            msg += f'\n<b>├ETA:</b> {download.eta()}'
             if hasattr(download, 'seeders_num'):
                 try:
-                    msg += f"\n<b>├🐌Seeders: {download.seeders_num()} | 🌱Leechers: {download.leechers_num()}</b>"
+                    msg += f"\n<b>├S/L: {download.seeders_num()}/{download.leechers_num()}</b>"
                 except:
                     pass
         elif download.status() == MirrorStatus.STATUS_SEEDING:
